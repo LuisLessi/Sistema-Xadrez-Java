4fe060626677e8b8c8c108d0ff805c5ef4a54cda
LuisLessi <luisctiunesp@hotmail.com>
2020-08-03 22:01:05 -0300

    criação Primeira classe: Position

---
+++ src/bordLayer/Position.java
@@ -0,0 +1,35 @@
+package bordLayer;
+
+public final class Position {
+	private Integer row;
+	private Integer column;
+	
+	public Position() {
+	}
+
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	public void setValue(int row, int column) {
+		this.column = column;
+		this.row = row;
+	}
+
+	@Override
+	public String toString() {
+		return this.row+", "+ this.column;
+	}
+	
+	
+	
+}
