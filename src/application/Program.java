0f4b5b5319625fafbd7727cbfb5e5503865eb445
LuisLessi <luisctiunesp@hotmail.com>
2020-08-05 14:24:37 -0300

    Movimentação das peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 import boardLayer.Board;
 import chessLayer.ChessMatch;
+import chessLayer.ChessPiece;
+import chessLayer.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
 		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		System.out.println();
+		System.out.print("Target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+	}
 	}
 
 }
